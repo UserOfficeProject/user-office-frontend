6ca33e5c842529a2e24dfa37f2519964c57886e5
Peter Asztalos <peter.asztalos@sigmatechnology.se>
2020-11-23 13:33:45 +0100

    Added Excel export support for SEP

--- src/components/SEP/MeetingComponents/SEPMeetingComponentsView.tsx
+++ src/components/SEP/MeetingComponents/SEPMeetingComponentsView.tsx
@@ -1,9 +1,14 @@
+import IconButton from '@material-ui/core/IconButton';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import Tooltip from '@material-ui/core/Tooltip';
+import GridOnIcon from '@material-ui/icons/GridOn';
 import { Options, MTableToolbar } from 'material-table';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 
 import SelectedCallFilter from 'components/common/SelectedCallFilter';
 import { useCallsData } from 'hooks/call/useCallsData';
+import { useDownloadXLSXSEP } from 'hooks/SEP/useDownloadXLSXSEP';
 
 import SEPMeetingInstrumentsTable from './SEPMeetingInstrumentsTable';
 
@@ -11,9 +16,17 @@ type SEPMeetingComponentsViewProps = {
   sepId: number;
 };
 
+const useStyles = makeStyles(theme => ({
+  spacing: {
+    margin: theme.spacing(1),
+  },
+}));
+
 const SEPMeetingComponentsView: React.FC<SEPMeetingComponentsViewProps> = ({
   sepId,
 }) => {
+  const classes = useStyles();
+  const downloadSEPXLSX = useDownloadXLSXSEP();
   const { loadingCalls, calls } = useCallsData();
   // NOTE: Default call is with id=1
   const [selectedCallId, setSelectedCallId] = useState<number>(1);
@@ -29,6 +42,15 @@ const SEPMeetingComponentsView: React.FC<SEPMeetingComponentsViewProps> = ({
           onChange={setSelectedCallId}
           callId={selectedCallId}
         />
+        <Tooltip title="Export in Excel">
+          <IconButton
+            aria-label="export in excel"
+            className={classes.spacing}
+            onClick={() => downloadSEPXLSX(sepId, selectedCallId)}
+          >
+            <GridOnIcon />
+          </IconButton>
+        </Tooltip>
       </>
     );
 
