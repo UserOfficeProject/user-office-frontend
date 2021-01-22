a03b91f8058e4242b94ab2cd8dae20306f4aecc4
Peter Asztalos <peter.asztalos@sigmatechnology.se>
2021-01-22 16:21:12 +0100

    sdk updated

--- src/generated/sdk.ts
+++ src/generated/sdk.ts
@@ -1242,6 +1242,7 @@ export type ProposalsFilter = {
   callId?: Maybe<Scalars['Int']>;
   instrumentId?: Maybe<Scalars['Int']>;
   proposalStatusId?: Maybe<Scalars['Int']>;
+  shortCodes?: Maybe<Array<Scalars['String']>>;
 };
 
 export type ProposalsQueryResult = {
