2b11bcdb7717d322afd35dbd95f75c07186a968e
Peter Asztalos <peter.asztalos@sigmatechnology.se>
2021-01-22 14:06:16 +0100

    - e2e tests added - small fixes

--- src/components/questionary/questionaryComponents/DatePicker/QuestionDateForm.tsx
+++ src/components/questionary/questionaryComponents/DatePicker/QuestionDateForm.tsx
@@ -1,9 +1,12 @@
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { Field } from 'formik';
 import { TextField } from 'formik-material-ui';
 import React from 'react';
 import * as Yup from 'yup';
 
 import FormikUICustomCheckbox from 'components/common/FormikUICustomCheckbox';
+import FormikUICustomDatePicker from 'components/common/FormikUICustomDatePicker';
 import TitledContainer from 'components/common/TitledContainer';
 import { FormComponent } from 'components/questionary/QuestionaryComponentRegistry';
 import { Question } from 'generated/sdk';
@@ -55,6 +58,7 @@ export const QuestionDateForm: FormComponent<Question> = props => {
             fullWidth
             inputProps={{ 'data-cy': 'tooltip' }}
           />
+
           <TitledContainer label="Constraints">
             <Field
               name="config.required"
@@ -64,6 +68,35 @@ export const QuestionDateForm: FormComponent<Question> = props => {
               fullWidth
               data-cy="required"
             />
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Field
+                name="config.minDate"
+                label="Min"
+                format="yyyy-MM-dd"
+                component={FormikUICustomDatePicker}
+                margin="normal"
+                fullWidth
+                data-cy="minDate"
+              />
+              <Field
+                name="config.maxDate"
+                label="Max"
+                format="yyyy-MM-dd"
+                component={FormikUICustomDatePicker}
+                margin="normal"
+                fullWidth
+                data-cy="maxDate"
+              />
+              <Field
+                name="config.defaultDate"
+                label="Default"
+                format="yyyy-MM-dd"
+                component={FormikUICustomDatePicker}
+                margin="normal"
+                fullWidth
+                data-cy="defaultDate"
+              />
+            </MuiPickersUtilsProvider>
           </TitledContainer>
         </>
       )}
--- src/components/questionary/questionaryComponents/DatePicker/QuestionTemplateRelationDateForm.tsx
+++ src/components/questionary/questionaryComponents/DatePicker/QuestionTemplateRelationDateForm.tsx
@@ -2,11 +2,11 @@ import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { Field } from 'formik';
 import { TextField } from 'formik-material-ui';
-import { KeyboardDatePicker } from 'formik-material-ui-pickers';
 import React from 'react';
 import * as Yup from 'yup';
 
 import FormikUICustomCheckbox from 'components/common/FormikUICustomCheckbox';
+import FormikUICustomDatePicker from 'components/common/FormikUICustomDatePicker';
 import FormikUICustomDependencySelector from 'components/common/FormikUICustomDependencySelector';
 import TitledContainer from 'components/common/TitledContainer';
 import { FormComponent } from 'components/questionary/QuestionaryComponentRegistry';
@@ -51,28 +51,28 @@ export const QuestionTemplateRelationDateForm: FormComponent<QuestionTemplateRel
                 name="config.minDate"
                 label="Min"
                 format="yyyy-MM-dd"
-                component={KeyboardDatePicker}
+                component={FormikUICustomDatePicker}
                 margin="normal"
                 fullWidth
-                data-cy="tooltip"
+                data-cy="minDate"
               />
               <Field
                 name="config.maxDate"
                 label="Max"
                 format="yyyy-MM-dd"
-                component={KeyboardDatePicker}
+                component={FormikUICustomDatePicker}
                 margin="normal"
                 fullWidth
-                data-cy="tooltip"
+                data-cy="maxDate"
               />
               <Field
                 name="config.defaultDate"
                 label="Default"
                 format="yyyy-MM-dd"
-                component={KeyboardDatePicker}
+                component={FormikUICustomDatePicker}
                 margin="normal"
                 fullWidth
-                data-cy="tooltip"
+                data-cy="defaultDate"
               />
             </MuiPickersUtilsProvider>
           </TitledContainer>
