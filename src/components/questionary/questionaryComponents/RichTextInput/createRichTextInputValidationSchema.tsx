21d41aa888d6563841952b0e53ec35510f4cafc2
Martin Trajanovski <martin.trajanovski@ess.eu>
2021-02-23 10:36:35 +0100

    clean some unnecessary code and add some description for code blocks

--- src/components/questionary/questionaryComponents/RichTextInput/createRichTextInputValidationSchema.tsx
+++ src/components/questionary/questionaryComponents/RichTextInput/createRichTextInputValidationSchema.tsx
@@ -3,28 +3,34 @@ import * as Yup from 'yup';
 import { QuestionaryComponentDefinition } from 'components/questionary/QuestionaryComponentRegistry';
 import { RichTextInputConfig } from 'generated/sdk';
 
-const decodeHtml = (html: string) => {
-  const txt = document.createElement('textarea');
-  txt.innerHTML = html;
+const getRawTextFromHtml = (html: string) => {
+  const div = document.createElement('div');
+  div.innerHTML = html;
 
-  return txt.value;
+  return div.textContent || div.innerText || '';
 };
 
-const stripHtmlTags = (htmlString: string) => {
-  const decoded = decodeHtml(htmlString);
+const stripHtmlTagsAndCleanText = (htmlString: string) => {
+  // NOTE: Get raw text from html
+  const rawText = getRawTextFromHtml(htmlString);
 
-  const decodedStripped = decoded
-    .replace(/(<([^>]+)>)/gi, '')
+  /**
+   * NOTE:
+   * 1. Remove all newline characters from counting.
+   * 2. Replace the surrogate pairs(emojis) with _ and count them as one character instead of two ("😀".length = 2).
+   *    https://stackoverflow.com/questions/31986614/what-is-a-surrogate-pair
+   */
+  const strippedCleanedText = rawText
     .replace(/(\r\n|\n|\r)/gm, '')
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, '_')
     .trim();
 
-  return decodedStripped;
+  return strippedCleanedText;
 };
 
 export const createRichTextInputValidationSchema: QuestionaryComponentDefinition['createYupValidationSchema'] = answer => {
   let schema = Yup.string().transform(function(value: string) {
-    return stripHtmlTags(value);
+    return stripHtmlTagsAndCleanText(value);
   });
 
   const config = answer.config as RichTextInputConfig;
@@ -33,11 +39,12 @@ export const createRichTextInputValidationSchema: QuestionaryComponentDefinition
     schema = schema.required(`This is a required field`);
   }
 
-  config.max &&
-    (schema = schema.max(
+  if (config.max) {
+    schema = schema.max(
       config.max,
       `Value must be at most ${config.max} characters`
-    ));
+    );
+  }
 
   return schema;
 };
