cbb11689efaedcc02bbfcebc3accfd74109a00fd
jekabskarklins <jekabs.karklins@ess.eu>
2021-04-21 15:24:09 +0200

    Fetching proposal shortcode for the sample

--- src/hooks/sample/useSamplesWithQuestionaryStatus.ts
+++ src/hooks/sample/useSamplesWithQuestionaryStatus.ts
@@ -1,30 +1,28 @@
 import { useEffect, useState } from 'react';
 
+import { SamplesFilter } from 'generated/sdk';
 import { useDataApi } from 'hooks/common/useDataApi';
-import { SampleWithQuestionaryStatus } from 'models/Sample';
 
-export function useProposalSamples(proposalId: number | null) {
+import { SampleWithQuestionaryStatus } from '../../models/Sample';
+
+export function useSamplesWithQuestionaryStatus(filter?: SamplesFilter) {
   const [samples, setSamples] = useState<SampleWithQuestionaryStatus[]>([]);
 
+  const [samplesFilter, setSamplesFilter] = useState(filter);
   const [loadingSamples, setLoadingSamples] = useState(false);
   const api = useDataApi();
 
   useEffect(() => {
-    if (!proposalId) {
-      setSamples([]);
-
-      return;
-    }
     setLoadingSamples(true);
     api()
-      .getSamplesWithQuestionaryStatus({ filter: { proposalId } })
+      .getSamplesWithQuestionaryStatus({ filter: samplesFilter })
       .then((data) => {
         if (data.samples) {
           setSamples(data.samples);
         }
         setLoadingSamples(false);
       });
-  }, [api, proposalId]);
+  }, [api, samplesFilter]);
 
-  return { samples, loadingSamples, setSamples };
+  return { samples, loadingSamples, setSamples, setSamplesFilter };
 }
