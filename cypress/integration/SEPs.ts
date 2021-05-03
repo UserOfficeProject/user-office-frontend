bbe0e1409e9fa0c4ef8e4ca4135c0bd09d3f5537
Martin Trajanovski <martin.trajanovski@ess.eu>
2021-05-03 14:59:22 +0200

    still fixing tests

--- cypress/integration/SEPs.ts
+++ cypress/integration/SEPs.ts
@@ -587,16 +587,6 @@ context(
       cy.finishedLoading();
 
       cy.get('[data-cy="sep-assignments-table"] thead').contains('Deviation');
-
-      cy.contains('Meeting Components').click();
-
-      cy.finishedLoading();
-
-      cy.get("[title='Show proposals']").first().click();
-
-      cy.get('[data-cy="sep-instrument-proposals-table"] thead').contains(
-        'Deviation'
-      );
     });
 
     it('Officer should be able to assign SEP member to proposal in existing SEP', () => {
