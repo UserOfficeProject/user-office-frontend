cb4e839b502aebdfee1d6d6aaac11d6f60d45f57
jekabskarklins <jekabs.karklins@ess.eu>
2021-05-25 14:11:25 +0200

    Fix ProposalAdministration Tests

--- src/components/questionary/questionaryComponents/DatePicker/DateAnswerRenderer.tsx
+++ src/components/questionary/questionaryComponents/DatePicker/DateAnswerRenderer.tsx
@@ -4,6 +4,10 @@ import { AnswerRenderer } from 'components/questionary/QuestionaryComponentRegis
 import { toTzLessDateTime } from 'utils/Time';
 
 const DateAnswerRenderer: AnswerRenderer = ({ value }) => {
+  if (!value) {
+    return <span>Left blank</span>;
+  }
+
   return <span>{toTzLessDateTime(value)}</span>;
 };
 
