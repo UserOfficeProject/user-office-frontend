391ca5843bbedb6ad7c899a38d630aaa415784f4
jekabskarklins <jekabs.karklins@ess.eu>
2021-07-28 13:08:28 +0300

    cleanup

--- src/components/proposalBooking/UserUpcomingExperimentsTable.tsx
+++ src/components/proposalBooking/UserUpcomingExperimentsTable.tsx
@@ -39,7 +39,6 @@ export default function UserUpcomingExperimentsTable() {
       setModalContents(null);
     },
     eventUpdated: (updatedEvent) => {
-      console.log('event Updatd');
       const updatedEvents = proposalScheduledEvents.map((event) =>
         event?.id === updatedEvent?.id ? updatedEvent : event
       );
