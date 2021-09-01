6393534c2c8fed8679d2565401ca2cdc0fe60358
jekabskarklins <jekabs.karklins@ess.eu>
2021-09-01 11:58:08 +0300

    cleanup tests

--- cypress/integration/risk_assessments.ts
+++ cypress/integration/risk_assessments.ts
@@ -1,18 +1,13 @@
 import faker from 'faker';
 
-faker.seed(1);
-
 const coProposerName = 'Benjamin';
 const coProposerEmail = 'ben@inbox.com';
-const visitorName = 'Dawson';
 const visitorEmail = 'david@teleworm.us';
 
 const questionTitle = faker.lorem.words(3);
 const answer = faker.lorem.words(3);
 
-const samplesTemplateName = faker.lorem.words(3);
-const samplesTopicName = faker.lorem.words(3);
-const samplesTitle = faker.lorem.words(3);
+const sampleTitle = /My sample title/i;
 
 const proposalTitle = 'Test proposal';
 const riskAssessmentButtonTitle = 'Finish risk assessment';
@@ -25,27 +20,9 @@ context('visits tests', () => {
     cy.resetSchedulerDB(true);
     // Add co-proposer
     cy.login('officer');
-    /*
-    cy.createTemplate('sample', samplesTemplateName, '');
-
-    cy.navigateToTemplatesSubmenu('Proposal templates');
-    cy.contains('default template').parent().get("[title='Edit']").click();
-    cy.createTopic(samplesTopicName);
-    cy.createSampleQuestion('Samples question', samplesTemplateName);
-
-    cy.visit('/');
-*/
     cy.contains('999999').parent().find('[title="View proposal"]').click();
     cy.get('[data-cy=toggle-edit-proposal]').click();
-    /*
-    cy.contains(samplesTopicName).click();
-    cy.get('[data-cy=add-button]').click();
-    cy.get('[data-cy=title-input] input').type(samplesTitle);
-    cy.get(
-      '[data-cy=sample-declaration-modal] [data-cy=save-and-continue-button]'
-    ).click();
-    cy.finishedLoading();
-*/
+
     cy.get('[data-cy=questionary-stepper]').contains('New proposal').click();
     cy.get('[data-cy=add-participant-button]').click();
     cy.contains(coProposerName).parent().find('[type=checkbox]').click();
@@ -111,9 +88,7 @@ context('visits tests', () => {
     cy.login('user');
     cy.get(`[title='${riskAssessmentButtonTitle}']`).click();
     cy.get('[data-cy=samples-dropdown]').click();
-    cy.get('[role=listbox]')
-      .contains(/My sample title/i)
-      .click();
+    cy.get('[role=listbox]').contains(sampleTitle).click();
 
     cy.get('body').type('{esc}');
     cy.contains(questionTitle).then(($elem: any) => {
