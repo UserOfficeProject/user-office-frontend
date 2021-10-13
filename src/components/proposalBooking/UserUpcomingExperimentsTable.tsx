efafded49d80c5a10781e530f190937abe5c7fd6
jekabskarklins <jekabs.karklins@ess.eu>
2021-10-13 16:58:33 +0200

    fix tests

--- src/components/proposalBooking/UserUpcomingExperimentsTable.tsx
+++ src/components/proposalBooking/UserUpcomingExperimentsTable.tsx
@@ -76,7 +76,7 @@ export default function UserUpcomingExperimentsTable() {
   }
 
   return (
-    <Grid item xs={12}>
+    <Grid item xs={12} data-cy="upcoming-experiments">
       <StyledPaper margin={[0]}>
         <MaterialTable
           actions={[
