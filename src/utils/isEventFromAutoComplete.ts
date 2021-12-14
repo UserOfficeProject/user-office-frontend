146eff324fee342ae966c370a5a1a25512c5fbf6
jekabskarklins <jekabs.karklins@ess.eu>
2021-12-14 12:38:05 +0100

    Avoid using any

--- src/utils/isEventFromAutoComplete.ts
+++ src/utils/isEventFromAutoComplete.ts
@@ -2,6 +2,6 @@ import { BaseSyntheticEvent } from 'react';
 
 // Addressing the bug https://github.com/jaredpalmer/formik/issues/721
 const isEventFromAutoComplete = (event: BaseSyntheticEvent) =>
-  (event.nativeEvent as any).view === undefined;
+  (event.nativeEvent as Record<string, unknown>).view === undefined;
 
 export default isEventFromAutoComplete;
