866ee7e158345af171a477e00fe893467c1a2c62
jekabskarklins <jekabs.karklins@ess.eu>
2022-03-03 13:38:32 +0100

    fix: nullchecks

--- src/components/proposal/ProposalTable.tsx
+++ src/components/proposal/ProposalTable.tsx
@@ -102,7 +102,7 @@ const ProposalTable = ({
       proposalsToClonePk: [proposalToClonePk],
     });
 
-    const [resultProposal] = result.cloneProposals.proposals;
+    const [resultProposal] = result.cloneProposals.proposals ?? [];
 
     if (
       !result.cloneProposals.rejection &&
