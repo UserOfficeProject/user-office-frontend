6605dd8614986981b2ca48e18202607cb1dce0bd
Martin Trajanovski <martin.trajanovski@gmail.com>
2022-03-07 16:54:22 +0100

    fix pre-commit linting

--- src/components/template/ProposalTemplatesTable.tsx
+++ src/components/template/ProposalTemplatesTable.tsx
@@ -95,7 +95,7 @@ function ProposalTemplatesTable(props: ProposalTemplatesTableProps) {
   );
 
   // NOTE: Keeping the columns inside the component just because it needs NumberOfCalls which is wrapped with callback and uses setSelectedTemplateId.
-  const columnss: Column<ProposalTemplateRowDataType>[] = [
+  const columns: Column<ProposalTemplateRowDataType>[] = [
     { title: 'Name', field: 'name' },
     { title: 'Description', field: 'description' },
     { title: '# proposals', field: 'questionaryCount' },
