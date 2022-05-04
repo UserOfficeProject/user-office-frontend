9f6ee6b7f42c651da20eae0f55dbaa068f9b78a2
Martin Trajanovski <martin.trajanovski@gmail.com>
2022-05-04 09:22:55 +0200

    fix: Move before:run hook to the support instead of plugins

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -55,9 +55,4 @@ module.exports = (on: Cypress.PluginEvents) => {
       });
     });
   });
-
-  // NOTE: Get and store the settings into localStorage before running the tests. For now this is needed because of the "getFormats()" function used in initialDBData.
-  on('before:run', () => {
-    cy.getAndStoreAppSettings();
-  });
 };
